b4bff3409f17e80ee54b810abe8a7eac6a08d875
fm1320 <fm1320@users.noreply.github.com>
2021-02-18 22:45:09 +0000

    Update streamlit_appv1.py

--- streamlit_appv1.py
+++ streamlit_appv1.py
@@ -68,7 +68,7 @@ def finder(text,user_assay):
       tmp1=re.findall(pattern1 %assay[i],text, flags=re.IGNORECASE)
       #st.write("Sentences that have the assay:" ,tmp1)
       if (len(tmp1)>0):
-       sentc.append(tmp1[0][i])
+       sentc.append(tmp1[i])
     res_list = [assay[j] for j in index]
     #print("Nuovo:", nuovo)
     st.write("The assays mentioned are: \n ", res_list)
