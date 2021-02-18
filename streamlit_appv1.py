6e4054c5667f1755873c277e266d2c51742e2cbc
fm1320 <fm1320@users.noreply.github.com>
2021-02-18 21:10:52 +0000

    Update streamlit_appv1.py

--- streamlit_appv1.py
+++ streamlit_appv1.py
@@ -73,11 +73,11 @@ def finder(text,user_assay):
     #print("Nuovo:", nuovo)
     st.write("The assays mentioned are: \n ", res_list)
     st.write("The sentences that have an assay:")
-    st.write(type(sentc),type(sentc[1]))
     s=set(sentc)
     sentc=list(s)
-    for j in range(len(sentc)):
-            st.write(sentc[j])
+    st.write(type(sentc),type(sentc[1]))
+    #for j in range(len(sentc)-1):
+    #        st.write(sentc[j])
     return 0
 
 def main():
