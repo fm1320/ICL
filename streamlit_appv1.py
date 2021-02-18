80b277dd17a42c047485411268c419a5ae9779d2
fm1320 <fm1320@users.noreply.github.com>
2021-02-18 19:56:16 +0000

    Update streamlit_appv1.py

--- streamlit_appv1.py
+++ streamlit_appv1.py
@@ -112,7 +112,7 @@ def main():
       elif sel=="Regex":
          r_text = st.text_area("Enter text for entity recognition with Regex","Text here")
          iz=finder(r_text,"")
-		 st.write("Sentences with ASSAY:",iz)
+         st.write("Sentences with ASSAY:",iz)
       method = st.sidebar.selectbox("Choose input method (recommended:text box)", ["Text box", "URL"])   
 
       
